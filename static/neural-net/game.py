bcea0c7811ac5f11fea16ddf3f26e776ab0e7601
der-john <john@schmid-do.de>
2017-11-01 22:39:23 +0100

    fix maketrans

--- static/neural-net/game.py
+++ static/neural-net/game.py
@@ -28,7 +28,7 @@ def get_model_from_pickle(fn):
     return predict
 
 strip_whitespace = re.compile(r"\s+")
-translate_pieces = string.maketrans(".pnbrqkPNBRQK", "\x00" + "\x01\x02\x03\x04\x05\x06" + "\x08\x09\x0a\x0b\x0c\x0d")
+translate_pieces = str.maketrans(".pnbrqkPNBRQK", "\x00" + "\x01\x02\x03\x04\x05\x06" + "\x08\x09\x0a\x0b\x0c\x0d")
 
 def sf2array(pos, flip):
     # Create a numpy array from a sunfish representation
